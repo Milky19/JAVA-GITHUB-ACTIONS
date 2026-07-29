13e39b907c40149ef45f5bfbf117b9b84e7a7fd9
Milky19 <97377003+Milky19@users.noreply.github.com>
2026-07-29 20:23:43 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,9 +1,9 @@
 public class App {
 	public static void main(String[] args) {
 		int eno=1001;
-		String ename="KRISHNA"
-		int sal=10000
-		String course="DevSecOps"
+		String ename="KRISHNA";
+		int sal=10000;
+		String course="DevSecOps";
 		System.out.println("Learn Here and Lead Any where");
 		System.out.println("My emp no:" +eno);
 		System.out.println("My name is:" +ename);
