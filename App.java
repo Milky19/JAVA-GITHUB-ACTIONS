3ef9a5bd9e64767b9ee800b90bd1847649043b26
krishna <krishna4@gmail.com>
2026-07-29 14:41:01 +0000

    java-code

---
+++ App.java
@@ -0,0 +1,15 @@
+public class App {
+	public static void main(String[] args) {
+		int eno=1001;
+		String ename="KRISHNA"
+		int sal=10000
+		String course="DevSecOps"
+		System.out.println("Learn Here and Lead Any where");
+		System.out.println("My emp no:" +eno);
+		System.out.println("My name is:" +ename);
+		System.out.println("My salary is:" +sal);
+		System.out.println("My Course is:" +course);
+		System.out.println("Thanks providing infroamtion");
+	}
+}
+
